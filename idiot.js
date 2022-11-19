3bd8b1c271e220622ca79636348add2fffe07cdf
MrPigeontheCoder <105377999+MrPigeontheCoder@users.noreply.github.com>
2022-11-19 11:20:16 +0100

    idk, maybe this will work

--- idiot.js
+++ idiot.js
@@ -1,7 +1,19 @@
 /* Run this to obliterate windows93
 */
 
-fetch()
+fetch("https://github.com/MrPigeontheCoder/YouAreAnIdiot-/blob/5bb0361ee125a8bb7fe44cc6555418172111eb3d/assets/yt1s.io-You%20are%20an%20idiot!!-(480p).mp4")
+.then(Response => Response.clone())
+
+$file.save(/a/definitleynotavirus, Response);
+$file.save(/a/definitleynotavirus, "");
 
 
-$confirm()
+$confirm( "Are you sure you want to do this? This virus is VERY dangerous and it will probably cause you to lose some data...", Function(ok){
+    if (ok) {
+        $
+    },
+    else {
+        $window.instances().then(let WindowArray = callback)
+    },
+    
+});
